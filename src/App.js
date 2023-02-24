8562327127a9145dc0bb07d8a44f6399faf94530
yunhwan <sky10240107@naver.com>
2023-02-25 00:04:58 +0900

    chore: 이미지 에셋 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RoutesTest from "./components/RoutesTest";
+
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
@@ -11,13 +11,13 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/new" element={<New />}></Route>
           <Route path="/edit" element={<Edit />}></Route>
           <Route path="/diary/:id" element={<Diary />}></Route>
         </Routes>
-        <RoutesTest />
       </div>
     </BrowserRouter>
   );
