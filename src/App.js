15fae3cebe5113beca1ab78f5dc6cff58cf439da
yunhwan <sky10240107@naver.com>
2023-02-24 22:32:51 +0900

    feat: Home,New,Edit,Diary 페이지 생성 및 이동 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoutesTest from "./components/RoutesTest";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new" element={<New />}></Route>
+          <Route path="/edit" element={<Edit />}></Route>
+          <Route path="/diary" element={<Diary />}></Route>
+        </Routes>
+        <RoutesTest />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
