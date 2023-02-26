8a72f1b0b4ba8e56ea0767532de5701306ba45c2
yunhwan <sky10240107@naver.com>
2023-02-26 13:43:32 +0900

    feat: 오늘의 일기 쓰는 기능 추가

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
@@ -40,6 +40,8 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const contentRef = useRef();
+  const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
@@ -80,6 +82,18 @@ const DiaryEditor = () => {
                 isSelected={it.emotion_id === emotion}
               />
             ))}
+          </div>
+        </section>
+
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
           </div>
         </section>
       </div>
