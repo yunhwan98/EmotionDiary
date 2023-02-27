6d858d7d2aced5a517cc9a186043e5b5d83dd7e6
yunhwan <sky10240107@naver.com>
2023-02-27 13:21:45 +0900

    feat: Edit 페이지 구현

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/new" element={<New />}></Route>
-              <Route path="/edit" element={<Edit />}></Route>
+              <Route path="/edit/:id" element={<Edit />}></Route>
               <Route path="/diary/:id" element={<Diary />}></Route>
             </Routes>
           </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,37 +1,36 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
+
 const Edit = () => {
-  //url의 params를 받아오기
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [originData, setOriginData] = useState();
   //페이지 이동가능 하게하기
   const navigate = useNavigate();
+  const { id } = useParams();
 
-  const id = searchParams.get("id");
-  console.log("id : ", id);
+  const diaryList = useContext(DiaryStateContext);
 
-  const mode = searchParams.get("mode");
-  console.log("mode : ", mode);
+  //일기 데이터 꺼내오기
+  useEffect(() => {
+    if (diaryList.length > 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      //id 전달 여부에 따른 설정
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        //없는 id를 전달받으면 홈으로 이동(뒤로 가기x)
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
 
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정페이지 입니다.</p>
-      <button onClick={() => setSearchParams({ who: "yunhwan" })}>
-        QS 바꾸기
-      </button>
-      <button
-        onClick={() => {
-          navigate("home");
-        }}
-      >
-        Home으로 가기
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
